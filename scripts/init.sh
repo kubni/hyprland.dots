de2041e84d749ea27fd0ee715c4bfb508fce981e
Nikola Kuburović <nikola.kuburovic123@gmail.com>
2024-04-23 12:13:15 +0200

    Fix the relative paths in init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,6 @@ mkdir -p ~/.config/hypr
 mkdir -p ~/.config/waybar
 mkdir -p ~/.config/wofi
 
-cp hyprland.conf hyprload.toml hyprpaper.conf ~/.config/hypr/
-cp waybar/* ~/.config/waybar/
-cp wofi/* ~/.config/wofi/
+cp ../hyprland.conf ../hyprload.toml ../hyprpaper.conf ~/.config/hypr/
+cp ../waybar/* ~/.config/waybar/
+cp ../wofi/* ~/.config/wofi/
